c8be921b41ac0c547035d4f4d952812961fae3e6
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-10-11 03:41:37 +0200

    removal of old info system removal of sfml info helper update of cmakelists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,17 @@
 using namespace std;
 using nsRS::ResourceManager;
 
+
+namespace
+{
+
+}
 int main ()
 {
+
     cout << "Hello, World!" << endl;
 
-    nsInformation::FilePath a, b, c, d;
-
-    nsInformation::InformationHolder e (GameObject());
-
-    a.path_to_file = b.path_to_file = c.path_to_file = d.path_to_file = "../img/blabla.png";
-    {
-        int i {0};
-        a.index = ++i;
-        b.index = ++i;
-        c.index = ++i;
-        d.index = ++i;
-    }
-
-
-
-    e << a << b << c << d;
-
-    ResourceManager *manager = ResourceManager::Instance ();
+    ResourceManager *RSmanager = ResourceManager::Instance ();
 
 
     sf::Window window(sf::VideoMode(800, 600), "My window");
